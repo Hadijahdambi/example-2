429483c7cf19cbe1e79519d2a0caeb130cc2fc9a
triza mbone <mmbonetrizer9@gmail.com>
2019-04-12 15:43:59 +0300

    adding calculator

---
+++ calculator.py
@@ -0,0 +1,29 @@
+def add (a,b):
+	c = a+b
+	print(c)
+
+
+def subtract(a,b):
+	c = a-b
+	print(c)
+
+def multiply(a,b):
+	c = a*b
+	print(c)
+
+def divide(a,b):
+	c = a/b
+	print(c)
+
+def module(a,b):
+	c = a%b
+	print(c)
+def percantage(a,b):
+	c =(a/b)*100
+	print(c)
+def whole number division (a,b):
+	c = a//b
+	print(c)
+def Exponent(a,b):
+	c = a**b
+	print(c)
